bc513866f309fcf7fc884e3d21a4b38d938911a2
arnest00 <53844657+arnest00@users.noreply.github.com>
2020-05-28 16:39:02 -0400

    create pet class and methods

--- js/index.js
+++ js/index.js
@@ -5,8 +5,50 @@ const formContainer = document.querySelector('#form-container'),
       petImg = document.querySelector('#pet-image'),
       feedBtn = document.querySelector('#feed-button'),
       playBtn = document.querySelector('#play-button'),
-      scoldBtn = document.querySelector('#scold-button'),
+      trainBtn = document.querySelector('#train-button'),
       petFeedback = document.querySelector('#pet-feedback');
+
+class Pet {
+  constructor(name, dexNum) {
+    this.name = name;
+    this.dexNum = dexNum;
+    this.level = 1;
+    this.exp = 0;
+    this.affection = 0;
+    this.hunger = 1;
+    this.boredom = 1;
+    this.trained = false;
+  };
+
+  feed() {
+    if (this.hunger > 0) {
+      this.hunger -= 1;
+      return `You fed ${this.name}!`;
+    } else {
+      return `${this.name} isn't hungry!`;
+    };
+  };
+
+  play() {
+    if (this.boredom > 0) {
+      this.boredom -= 1;
+      this.affection += 1;
+      return `You played with ${this.name}!`;
+    } else {
+      return `${this.name} doesn't feel like playing!`;
+    };
+  };
+
+  train() {
+    if (this.trained === false) {
+      this.trained = true;
+      this.exp += 1;
+      return `You trained with ${this.name}!`;
+    } else {
+      return `${this.name} is too tired to train!`;
+    }
+  };
+};
 
 petForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -14,21 +56,23 @@ petForm.addEventListener('submit', e => {
   const nameChoice = document.querySelector('#name').value,
         petChoice = document.querySelector('#pet').value;
 
-  petName.innerText = `Hello, ${nameChoice}!`;
-  petImg.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${petChoice}.png`);
+  const newPet = new Pet(nameChoice, petChoice);
+
+  petName.innerText = `${newPet.name}, Level ${newPet.level}`;
+  petImg.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${newPet.dexNum}.png`);
 
   formContainer.classList.add('hide');
   petContainer.classList.remove('hide');
 
   feedBtn.addEventListener('click', e => {
-    petFeedback.innerText = `You fed ${nameChoice}!`;
+    petFeedback.innerText = newPet.feed();
   });
 
   playBtn.addEventListener('click', e => {
-    petFeedback.innerText = `You played with ${nameChoice}!`;
+    petFeedback.innerText = newPet.play();
   });
 
-  scoldBtn.addEventListener('click', e => {
-    petFeedback.innerText = `You scolded ${nameChoice}!`;
+  trainBtn.addEventListener('click', e => {
+    petFeedback.innerText = newPet.train();
   });
 });
